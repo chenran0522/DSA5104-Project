d18730ce3b33ac6e5abf640696a73ef5ece6e83c
Xaviersfk <55449155+Xaviersfk@users.noreply.github.com>
2023-10-23 20:28:14 +0800

    finish first step of building our backend

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -1,34 +1,48 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, json, request
+from bson import ObjectId
 import mysql.connector
 from pymongo import MongoClient
+
+
+def json_encoder(o):
+    if isinstance(o, ObjectId):
+        return str(o)
+    raise TypeError(
+        f"Object of type {type(o).__name__} is not JSON serializable")
+
 
 app = Flask(__name__)
 
 
-@app.route('/query', methods=['POST'])
-def query_databases():
-    query = request.json.get('query')
+@app.route('/search_image', methods=['POST'])
+def search_image():
+    title = request.json['title']
 
-    # 连接MySQL
-    mysql_conn = mysql.connector.connect(
-        user='username', password='password', host='127.0.0.1', database='yourdatabase')
-    mysql_cursor = mysql_conn.cursor()
-    mysql_cursor.execute(query)
-    mysql_results = mysql_cursor.fetchall()
-    mysql_conn.close()
+    # Fetch data from MySQL
+    mydb = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="Wxp-0525",
+        database="imagedb"
+    )
+    cursor = mydb.cursor()
+    cursor.execute(f"SELECT * FROM image WHERE name = '{title}'")
+    mysql_result = cursor.fetchone()
 
     # 连接MongoDB
     mongo_client = MongoClient('localhost', 27017)
-    mongo_db = mongo_client['yourmongodb']
-    mongo_collection = mongo_db['yourcollection']
-    mongo_results = list(mongo_collection.find(query))
+    mongo_db = mongo_client['DSA5104']
+    mongo_collection = mongo_db['image']
+    mongo_results = list(mongo_collection.find({"title": title}))
+    data = {
+        "mysql_data": mysql_result,
+        "mongo_data": mongo_results
+    }
+    return app.response_class(
+        response=json.dumps(data, default=json_encoder),
+        mimetype='application/json'
+    )
 
-    # 返回两个数据库的查询结果
-    return jsonify({
-        'mysql': mysql_results,
-        'mongodb': mongo_results
-    })
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
