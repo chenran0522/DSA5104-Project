1b72bf1861d6d80c815a956aad1ff3c6ec8c08a8
chenran0522 <chenran0522@gmail.com>
2023-10-24 15:40:28 +0800

    1

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -25,7 +25,7 @@ def search_image():
     mydb = mysql.connector.connect(
         host="127.0.0.1",
         user="root",
-        password="Wxp-0525",
+        password="cr003265...",
         database="imagedb"
     )
     cursor = mydb.cursor()
@@ -37,6 +37,66 @@ def search_image():
     mongo_db = mongo_client['DSA5104']
     mongo_collection = mongo_db['image']
     mongo_results = list(mongo_collection.find({"title": title}))
+    data = {
+        "mysql_data": mysql_result,
+        "mongo_data": mongo_results
+    }
+    return app.response_class(
+        response=json.dumps(data, default=json_encoder),
+        mimetype='application/json'
+    )
+
+@app.route('/search_image_by_id', methods=['POST'])
+def search_image_by_id():
+    image_id = request.json['image_id']
+
+    # Fetch data from MySQL
+    mydb = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="cr003265...",
+        database="imagedb"
+    )
+    cursor = mydb.cursor()
+    cursor.execute(f"SELECT * FROM image WHERE image_id = '{image_id}'")
+    #cursor.execute(f"SELECT * FROM image WHERE name = '{title}'")
+    mysql_result = cursor.fetchone()
+
+    # 连接MongoDB
+    mongo_client = MongoClient('localhost', 27017)
+    mongo_db = mongo_client['DSA5104']
+    mongo_collection = mongo_db['image']
+    mongo_results = list(mongo_collection.find({"id": image_id}))
+    data = {
+        "mysql_data": mysql_result,
+        "mongo_data": mongo_results
+    }
+    return app.response_class(
+        response=json.dumps(data, default=json_encoder),
+        mimetype='application/json'
+    )
+
+@app.route('/search_image_by_id', methods=['POST'])
+def search_image_by_id():
+    image_id = request.json['image_id']
+
+    # Fetch data from MySQL
+    mydb = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="cr003265...",
+        database="imagedb"
+    )
+    cursor = mydb.cursor()
+    cursor.execute(f"SELECT * FROM image WHERE image_id = '{image_id}'")
+    #cursor.execute(f"SELECT * FROM image WHERE name = '{title}'")
+    mysql_result = cursor.fetchone()
+
+    # 连接MongoDB
+    mongo_client = MongoClient('localhost', 27017)
+    mongo_db = mongo_client['DSA5104']
+    mongo_collection = mongo_db['image']
+    mongo_results = list(mongo_collection.find({"id": image_id}))
     data = {
         "mysql_data": mysql_result,
         "mongo_data": mongo_results
