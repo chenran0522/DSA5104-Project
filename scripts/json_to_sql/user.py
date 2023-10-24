fadd571d61af69d0276ac528dafada6d54c5152d
RanbowbbHuxiaolin <130633921+RanbowbbHuxiaolin@users.noreply.github.com>
2023-10-24 21:01:08 +0800

    Update user.py

--- scripts/json_to_sql/user.py
+++ scripts/json_to_sql/user.py
@@ -15,15 +15,21 @@ conn = mysql.connector.connect(**config)
 cursor = conn.cursor()
 
 with open("../../data/json/user.json", 'r') as file:
-    for line in file:
-        data = json.loads(line.strip())
+    data = json_file.read()
 
-        # 插入Image数据
-        cursor.execute("INSERT IGNORE INTO User (user_id, password, name) VALUES (%s, %s, %s)",
-                       (data['user_id'], data['password'], data['name']))
+# 使用正则表达式查找大括号中的数据块
+pattern = r'\{[^{}]*\}'
+matches = re.findall(pattern, data)
 
-        # 提交事务
-        conn.commit()
+for line in matches:
+    data = json.loads(line.strip())
+
+    # 插入Image数据
+    cursor.execute("INSERT IGNORE INTO User (user_id, password, name) VALUES (%s, %s, %s)",
+                   (data['user_id'], data['password'], data['name']))
+
+    # 提交事务
+    conn.commit()
 
 # 关闭连接
 cursor.close()
