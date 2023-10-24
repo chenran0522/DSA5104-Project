53d0b843b56a9aa388cfbf2e168d038889a37a85
RanbowbbHuxiaolin <130633921+RanbowbbHuxiaolin@users.noreply.github.com>
2023-10-24 19:19:13 +0800

    Create user.py

---
+++ scripts/json_to_sql/user.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import json
+
+# 数据库连接配置
+config = {
+    'user': 'root',
+    'password': 'cr003265...',
+    'host': '127.0.0.1',
+    'database': 'userDB',
+    'raise_on_warnings': True
+}   
+
+# 与MySQL建立连接
+conn = mysql.connector.connect(**config)
+cursor = conn.cursor()
+
+with open("../../data/json/user.json", 'r') as file:
+    for line in file:
+        data = json.loads(line.strip())
+
+        # 插入Image数据
+        cursor.execute("INSERT IGNORE INTO User (user_id, password, name) VALUES (%s, %s, %s)",
+                       (data['user_id'], data['password'], data['name']))
+
+        # 提交事务
+        conn.commit()
+
+# 关闭连接
+cursor.close()
+conn.close()
