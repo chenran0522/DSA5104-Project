a6636ed52da6986272877175721c6a4fb86ba2ab
JesseLwy <148430602+JesseLwy@users.noreply.github.com>
2023-10-25 01:40:09 +0800

    Add files via upload

---
+++ scripts/MySql/music_table.sql
@@ -0,0 +1,44 @@
+-- create DATABASE MusicDB;
+USE MusicDB;
+
+-- 删除已存在的表，如果需要的话
+DROP TABLE IF EXISTS Music_Artist;
+DROP TABLE IF EXISTS Music_genres;
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Music;
+DROP TABLE IF EXISTS genres;
+
+-- 重新创建表
+CREATE TABLE Music (
+    music_id INT PRIMARY KEY,
+    music_name VARCHAR(255),
+    rating FLOAT,
+    play_count INT,
+    type_id INT
+);
+
+CREATE TABLE genres (
+    genre_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) unique
+);
+
+CREATE TABLE Music_genres (
+    music_id INT,
+    genre_id INT,
+    PRIMARY KEY (music_id, genre_id),
+    FOREIGN KEY (music_id) REFERENCES Music(music_id),
+    FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+);
+
+CREATE TABLE Artist (
+    artist_id INT AUTO_INCREMENT PRIMARY KEY,
+    artist_name VARCHAR(255) unique
+);
+
+CREATE TABLE Music_Artist (
+    music_id INT,
+    artist_id INT,
+    PRIMARY KEY (music_id, artist_id),
+    FOREIGN KEY (music_id) REFERENCES Music(music_id),
+    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
+);
