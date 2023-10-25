c9b6c465a1e00209835322505eac6fcb814a700a
Xaviersfk <55449155+Xaviersfk@users.noreply.github.com>
2023-10-25 15:19:08 +0800

    change image.py

--- scripts/json_to_sql/image.py
+++ scripts/json_to_sql/image.py
@@ -4,9 +4,9 @@ import json
 # 数据库连接配置
 config = {
     'user': 'root',
-    'password': '',
+    'password': 'cr003265...',
     'host': '127.0.0.1',
-    'database': 'MusicDB',
+    'database': 'ImageDB',
     'raise_on_warnings': True
 }   
 
@@ -16,15 +16,14 @@ cursor = conn.cursor()
 
 # 用于存储已插入的genres和cast，避免重复插入
 inserted_genres = set()
-inserted_artist_names = set()
-
-with open('C:/Users/liwei/Desktop/dsa5104/project/music/music.json', 'r') as file:
+inserted_casts = set()
+with open("../../data/json/image.json", 'r') as file:
     for line in file:
         data = json.loads(line.strip())
 
-        # 插入Music数据
-        cursor.execute("INSERT IGNORE INTO Music (music_id, music_name, rating, play_count, type_id) VALUES (%s, %s, %s, %s, %s)",
-                       (data['id'], data['music_name'], data['rating'], data['play_count'], data['type_id']))
+        # 插入Image数据
+        cursor.execute("INSERT IGNORE INTO Image (image_id, format, name, rating, watch_numbers, RUN_Time, type_id) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                       (data['id'], data['format'], data['title'], data['rating'], data['watch_numbers'], data['runtime'], data['type_id']))
 
         # 处理并插入genres数据
         genres = data.get('genres')
@@ -37,26 +36,26 @@ with open('C:/Users/liwei/Desktop/dsa5104/project/music/music.json', 'r') as fil
                 cursor.execute(
                     "SELECT genre_id FROM genres WHERE name = %s;", (genre,))
                 genre_id = cursor.fetchone()[0]
-                music_id = data['id']
+                image_id = data['id']
                 cursor.execute(
-                    "INSERT INTO Music_genres (music_id, genre_id) VALUES (%s, %s) ON DUPLICATE KEY UPDATE music_id=music_id, genre_id=genre_id",
-                    (music_id, genre_id))
+                    "INSERT INTO image_genre (image_id, genre_id) VALUES (%s, %s) ON DUPLICATE KEY UPDATE image_id=image_id, genre_id=genre_id",
+                    (image_id, genre_id))
 
         # 处理并插入cast数据
-        artist_list = data.get('artist_name')
-        if artist_list:
-            for artist_name in artist_list:
-                if artist_name not in inserted_artist_names :
+        cast_list = data.get('cast')
+        if cast_list:
+            for cast in cast_list:
+                if cast not in inserted_casts:
                     cursor.execute(
-                        "INSERT INTO  Artist(artist_name) VALUES (%s) ON DUPLICATE KEY UPDATE artist_name=artist_name",
-                        (artist_name,))
-                    inserted_artist_names.add(artist_name)
+                        "INSERT INTO cast_info (cast_name) VALUES (%s) ON DUPLICATE KEY UPDATE cast_name=cast_name",
+                        (cast,))
+                    inserted_casts.add(cast)
                 cursor.execute(
-                    "SELECT artist_id FROM Artist WHERE artist_name = %s;", (artist_name,))
-                artist_id = cursor.fetchone()[0]
+                    "SELECT cast_id FROM Cast_info WHERE cast_name = %s;", (cast,))
+                cast_id = cursor.fetchone()[0]
                 cursor.execute(
-                    "INSERT INTO Music_Artist (music_id, artist_id) VALUES (%s, %s) ON DUPLICATE KEY UPDATE music_id=music_id",
-                    (data['id'], artist_id))
+                    "INSERT INTO image_cast (image_id, cast_id) VALUES (%s, %s) ON DUPLICATE KEY UPDATE image_id=image_id",
+                    (data['id'], cast_id))
 
         # 提交事务
         conn.commit()
