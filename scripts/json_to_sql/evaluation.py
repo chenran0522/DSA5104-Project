4bee40623916ae2453865af31f4c03c9281440b0
RanbowbbHuxiaolin <130633921+RanbowbbHuxiaolin@users.noreply.github.com>
2023-10-31 16:57:09 +0800

    Update evaluation.py

--- scripts/json_to_sql/evaluation.py
+++ scripts/json_to_sql/evaluation.py
@@ -5,17 +5,17 @@ import re
 # 数据库连接配置
 config = {
     'user': 'root',
-    'password': 'Wxp-0525',
+    'password': 'ytx918107',
     'host': '127.0.0.1',
-    'database': 'SYSTEMDATABASE',
+    'database': 'userDB',
     'raise_on_warnings': True
-}
+}   
 
 # 与MySQL建立连接
 conn = mysql.connector.connect(**config)
 cursor = conn.cursor()
 
-with open("../../data/json/evaluation(3).json", "r", encoding="utf-8") as json_file:
+with open("../../data/json/evaluation.json", "r", encoding="utf-8") as json_file:
     data = json_file.read()
 
 # 使用正则表达式查找大括号中的数据块
@@ -26,7 +26,7 @@ for line in matches:
     data = json.loads(line.strip())
 
     # 插入Image数据
-    cursor.execute("INSERT IGNORE INTO Evaluation ( user_id, type_id, target_id, rating, comment) VALUES (%d, %d, %d, %f, %s)",
+    cursor.execute("INSERT IGNORE INTO Evaluation ( user_id, type_id, target_id, score, comment) VALUES (%s, %s, %s, %s, %s)",
                    (data['user_id'], data['type_id'], data['target_id'], data['score'], data['comment']))
 
     # 提交事务
