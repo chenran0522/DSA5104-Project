477451ce4aae238cde07ef750d6771384c5d9757
RanbowbbHuxiaolin <130633921+RanbowbbHuxiaolin@users.noreply.github.com>
2023-10-31 15:45:09 +0800

    Update Create_user_evaluation.sql

--- scripts/MySql/Create_user_evaluation.sql
+++ scripts/MySql/Create_user_evaluation.sql
@@ -1,12 +1,13 @@
 USE userdb;
 
 -- 删除已存在的表，如果需要的话
-DROP TABLE IF EXISTS User;
+
 DROP TABLE IF EXISTS Evaluation;
+DROP TABLE IF EXISTS User;
 
 -- 重新创建表
 CREATE TABLE User (
-    id INT PRIMARY KEY,
+    user_id INT PRIMARY KEY,
     name VARCHAR(255),
     password VARCHAR(255));
 
@@ -15,6 +16,8 @@ CREATE TABLE Evaluation (
     user_id INT,
     type_id INT,
     target_id INT,
+    score float,
+    comment VARCHAR(255),
     PRIMARY KEY (user_id, type_id, target_id),
-    FOREIGN KEY (user_id) REFERENCES User(id)
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
